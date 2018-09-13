36e7c78f5d472c1caa46f93d422972fbf0b5dc91
David Vogel <vogel@glue.ch>
2018-09-13 11:53:56 +0200

    first try

--- src/main/java/com/example/demoworkshop/DemoApplication.java
+++ src/main/java/com/example/demoworkshop/DemoApplication.java
@@ -9,4 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+		public static void test() {
+		// do nothing
+	}
 }
